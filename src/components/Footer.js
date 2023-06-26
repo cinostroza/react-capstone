dc86f4b86075d9b837a600d521ec0b96cf787947
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2023-06-26 17:03:10 -0400

    Adding basic css styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 const Footer = (props) => {
     return (
     <footer>
-        <img src={process.env.PUBLIC_URL + "/ll_big_logo.png"} atl="Little Lemon Logo"/>
-        <ul>
+        <div className="flex-container">
+        <img className="flex-item" src={process.env.PUBLIC_URL + "/ll_big_logo.png"} atl="Little Lemon Logo"/>
+        <ul className="flex-item">
             Dormant Navigation
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
@@ -11,18 +12,19 @@ const Footer = (props) => {
             <li><a href="#">Order Online</a></li>
             <li><a href="#">Login</a></li>
         </ul>
-        <ul>
+        <ul className="flex-item">
             Contact
             <li><a href="#">Address</a></li>
             <li><a href="#">Phone Number</a></li>
             <li><a href="#">Email</a></li>
         </ul>
-        <ul>
+        <ul className="flex-item">
             Social Media Links
             <li><a href="#">Address</a></li>
             <li><a href="#">Phone Number</a></li>
             <li><a href="#">Email</a></li>
         </ul>
+        </div>
     </footer>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,14 @@
 const Nav = (props) => {
     return (
     <nav>
-        <ul>
+        <ul className="flex-container" id="remove">
             <img src={process.env.PUBLIC_URL + "/Logo.svg"} alt="Little Lemon Logo"/>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Menu</a></li>
-            <li><a href="#">Reservations</a></li>
-            <li><a href="#">Order Online</a></li>
-            <li><a href="#">Login</a></li>
+            <li className="flex-item"><a href="#">Home</a></li>
+            <li className="flex-item"><a href="#">About</a></li>
+            <li className="flex-item"><a href="#">Menu</a></li>
+            <li className="flex-item"><a href="#">Reservations</a></li>
+            <li className="flex-item"><a href="#">Order Online</a></li>
+            <li className="flex-item"><a href="#">Login</a></li>
         </ul>
     </nav>
     )
